ec6a1012fdaa120841d2fd9d738e5116f9795e10
VOZ <voisfce@gmail.com>
2023-02-14 22:02:14 +0100

    Jeu de la fourchette Python

---
+++ PAP2223/python/jeu.py
@@ -0,0 +1,13 @@
+import random
+
+secret = random.randint(0,100)
+#print(secret)
+n=102 #pour rentrer dans la boucle
+while n != secret:
+    n= int(input("Entrez un nombre \n"))
+    if n<secret :
+        print("Le nombre est trop petit")
+    elif n>secret :
+        print("Le nombre est trop grand")
+print("Super tu as trouvé !!!")
+        
