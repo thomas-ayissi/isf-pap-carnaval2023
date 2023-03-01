c06a6f9ecf3e8a0720ea1751c52336763da73ccd
Thomas Ayissi <thomas8sea@gmail.com>
2023-03-01 21:52:26 +0100

    Etape 2 - DONE Amélioration 1 : Realisée Modifiez le code pour que l’on puisse quitter le jeu prématurément lorsque le joueur entre un nombre négatif .  Informez le joueur au début du jeu qu’il peut quitter le jeu en entrant un nombre négatif  Lorsqu’il quitte le jeu prématurément, affichez le message « Dommage … A la prochaine fois »

--- PAP2223/python/jeu.py
+++ PAP2223/python/jeu.py
@@ -4,7 +4,7 @@ secret = random.randint(0,100)
 #print(secret)
 n=102 #pour rentrer dans la boucle
 while n != secret:
-    n= int(input("Entrez un nombre \n"))
+    n= int(input("Entrez un nombre \n")
     if n<secret :
         print("Le nombre est trop petit")
     elif n>secret :
--- PAP2223/python/jeuV2.py
+++ PAP2223/python/jeuV2.py
@@ -1,12 +1,26 @@
 import random
 
+playerName="" #variable contenant le nom de la personne qui joue
+
+playerName= str(input("Bienvenue, quel est votre nom ?\n"))
+print("Bonjour"+ " " + playerName + ". "+ "Vous pouvez quitter le jeu en entrant un nombre négatif")
+
 secret = random.randint(0,100)
 #print(secret)
 n=102 #pour rentrer dans la boucle
+
 while n != secret:
     n= int(input("Entrez un nombre \n"))
-    if n<secret :
+
+    if ((n<secret) and (n>= 0)) :
         print("Le nombre est trop petit")
-    elif n>secret :
+    elif ((n>secret) and (n>= 0)) :
         print("Le nombre est trop grand")
-print("Super tu as trouvé !!!")
+    elif ((n!=secret) and (n<0)) :
+        print("Dommage … A la prochaine fois")
+        exit()
+    elif ((n==secret) and (n>= 0)) :
+        print("Super tu as trouvé !!!")
+
+
+
