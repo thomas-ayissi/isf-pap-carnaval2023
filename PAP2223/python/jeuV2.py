0fbd31693f2a6a74f59f0c81f8a5899e452dc822
Thomas Ayissi <thomas8sea@gmail.com>
2023-03-01 23:19:00 +0100

    Amélioration 2 : DONE +  Correction d'autres erreurs dans le code
    A la fin du jeu, le message doit reprendre le nombre de coups qu’il a fallu pour trouver le nombre
    secret. « Bravo, tu as trouvé le nombre …. en X coups »

--- PAP2223/python/jeuV2.py
+++ PAP2223/python/jeuV2.py
@@ -6,19 +6,29 @@ playerName = str(input("Bienvenue, quel est votre nom ?\n"))
 print("Bonjour" + " " + playerName + ". " + "Vous pouvez quitter le jeu en entrant un nombre négatif")
 
 secret = random.randint(0, 100)
-# print(secret)
+print(secret)
 
 # pour rentrer dans la boucle
 n = 102
+
+# mise en place du compteur
+counter = 1
+
 while n != secret:
     n = int(input("Entrez un nombre \n"))
 
     if (n < secret) and (n >= 0):
+        counter = counter + 1
         print("Le nombre est trop petit")
+
     elif (n > secret) and (n >= 0):
+        counter = counter + 1
         print("Le nombre est trop grand")
+
+
     elif (n != secret) and (n < 0):
-        print("Dommage … A la prochaine fois")
+        print("{}, tu as fait {} essais avant de choisir de quitter le jeu. Dommage a la prochaine fois" .format(playerName, counter))
         exit()
-    elif (n == secret) and (n >= 0):
-        print("Super tu as trouvé !!!")
+
+
+print("Bravo {}, tu as trouve le nombre {} en {} coups".format(playerName, secret, counter))
