945814461e28eeac8b0913643351753398cbe9f1
Thomas Ayissi <thomas8sea@gmail.com>
2023-03-01 22:10:19 +0100

    UPDATE Etape 2 - Correction des erreurs dans le code
    1/ Suppression d'une des doubles déclarations de la valeur "playerName"
    
    2/ Ajout des espaces devant les opératuers
    
    3/ Suppression des parenthèses surnuméraires, donc inutiles.

--- PAP2223/python/jeuV2.py
+++ PAP2223/python/jeuV2.py
@@ -1,26 +1,24 @@
 import random
 
-playerName="" #variable contenant le nom de la personne qui joue
+# variable contenant le nom de la personne qui joue
 
-playerName= str(input("Bienvenue, quel est votre nom ?\n"))
-print("Bonjour"+ " " + playerName + ". "+ "Vous pouvez quitter le jeu en entrant un nombre négatif")
+playerName = str(input("Bienvenue, quel est votre nom ?\n"))
+print("Bonjour" + " " + playerName + ". " + "Vous pouvez quitter le jeu en entrant un nombre négatif")
 
-secret = random.randint(0,100)
-#print(secret)
-n=102 #pour rentrer dans la boucle
+secret = random.randint(0, 100)
+# print(secret)
 
+# pour rentrer dans la boucle
+n = 102
 while n != secret:
-    n= int(input("Entrez un nombre \n"))
+    n = int(input("Entrez un nombre \n"))
 
-    if ((n<secret) and (n>= 0)) :
+    if (n < secret) and (n >= 0):
         print("Le nombre est trop petit")
-    elif ((n>secret) and (n>= 0)) :
+    elif (n > secret) and (n >= 0):
         print("Le nombre est trop grand")
-    elif ((n!=secret) and (n<0)) :
+    elif (n != secret) and (n < 0):
         print("Dommage … A la prochaine fois")
         exit()
-    elif ((n==secret) and (n>= 0)) :
+    elif (n == secret) and (n >= 0):
         print("Super tu as trouvé !!!")
-
-
-
