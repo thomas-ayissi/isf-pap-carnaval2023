dd4cceea05ad596355407aae200b98eaab8fca7a
Thomas Ayissi <thomas8sea@gmail.com>
2023-03-02 02:11:44 +0100

    Amélioration 3 : DONE
    Lorsqu’une partie est terminée, le programme doit demander au joueur de tapez « Y » ou « y » pour
    refaire une partie sinon le jeu s’arrête.
    
    Le programme affichera un message « C’était super,… A la prochaine fois »

--- PAP2223/python/jeuV2.py
+++ PAP2223/python/jeuV2.py
@@ -5,30 +5,36 @@ import random
 playerName = str(input("Bienvenue, quel est votre nom ?\n"))
 print("Bonjour" + " " + playerName + ". " + "Vous pouvez quitter le jeu en entrant un nombre négatif")
 
-secret = random.randint(0, 100)
-print(secret)
+# mise en place de la valeur permettant de recommencer une partie
+replayGame = "launch"
 
-# pour rentrer dans la boucle
-n = 102
+while replayGame == "launch" or replayGame == "y" or replayGame == "Y":
+    secret = random.randint(0, 100)
+    print(secret)
 
-# mise en place du compteur
-counter = 1
+    # pour rentrer dans la boucle
+    n = 102
 
-while n != secret:
-    n = int(input("Entrez un nombre \n"))
+    # mise en place du compteur
+    counter = 1
 
-    if (n < secret) and (n >= 0):
-        counter = counter + 1
-        print("Le nombre est trop petit")
+    while (replayGame != "") and (n != secret):
+        n = int(input("Entrez un nombre \n"))
+        if (n < secret) and (n >= 0):
+            counter = counter + 1
+            print("Le nombre est trop petit")
 
-    elif (n > secret) and (n >= 0):
-        counter = counter + 1
-        print("Le nombre est trop grand")
+        elif (n > secret) and (n >= 0):
+            counter = counter + 1
+            print("Le nombre est trop grand")
 
+        elif (n != secret) and (n < 0):
+            print("{}, tu as fait {} essais avant de choisir de quitter le jeu. Dommage a la prochaine fois" .format(playerName, counter))
+            exit()
 
-    elif (n != secret) and (n < 0):
-        print("{}, tu as fait {} essais avant de choisir de quitter le jeu. Dommage a la prochaine fois" .format(playerName, counter))
-        exit()
+        else:
+            print("Bravo {}, tu as trouve le nombre {} en {} coups".format(playerName, secret, counter))
+            replayGame = str(input("Tapez « Y » ou « y » pour refaire une partie sinon le jeu s’arrête\n"))
 
-
-print("Bravo {}, tu as trouve le nombre {} en {} coups".format(playerName, secret, counter))
+else:
+ print("C etait super {}, A la prochaine fois".format(playerName))
